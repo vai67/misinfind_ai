7f5bf20a63e005d190669726d8dc01aaecc81bf1
jazminuribe <jluribe2@illinois.edu>
2025-10-17 11:56:15 -0500

    Corrected gradiant background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        background: liner-gradient(90deg, #1CB5E0 0%, #000851 100%);
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the Misinformation Detector</h1>
         <p> Paste article below to check its credibilty!</p>
-        
+
         <input
           type="text"
           placeholder="Paste article here..."
@@ -20,7 +19,7 @@ function App() {
           style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px', backgroundColor: '#61dafb', border: 'none', borderRadius: '5px', cursor: 'pointer', fontWeight: 'bold' }}
         ></button>
 
-          <p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
